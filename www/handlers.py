1e3e7b3dbea411eaf74baa738144e94b13c6dfc3
hulksun <mumumumuxiansheng@gmail.com>
2017-04-07 20:26:51 +0800

    day-11

--- www/handlers.py
+++ www/handlers.py
@@ -8,14 +8,33 @@ import logging
 import hashlib
 import base64
 import asyncio
+import markdown2
 from coroweb import post, get
 from models import User, Comment, Blog, next_id
 from config import configs
 from apis import APIValueError
+from apis import APIError
+from apis import APIPermissionError
 from aiohttp import web
 
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
+
+
+def check_admin(request):
+    if request.__user__ is None or not request.__user__.admin:
+        raise APIPermissionError()
+
+
+def get_page_index(page_str):
+    p = 1
+    try:
+        p = int(page_str)
+    except ValueError:
+        pass
+    if p < 1:
+        p = 1
+    return p
 
 
 def user2cookie(user, max_age):
@@ -24,6 +43,11 @@ def user2cookie(user, max_age):
     s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
     L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
     return '-'.join(L)
+
+
+def text2html(text):
+    lines = map(lambda s: '<p>%s</p>' % s.replace('&', '&amp;').replace('<','&lt;').replace('>','&gt;'), filter(lambda s: s.strip() != '', text.strip('\n')))
+    return ' '.join(lines)
 
 
 @asyncio.coroutine
@@ -49,6 +73,22 @@ def cookie2user(cookie_str):
     except Exception as e:
         logging.exception(e)
         return None
+
+
+@get('/blog/{id}')
+def get_blog(id):
+    blog = yield from Blog.find(id)
+    comments = yield from Comment.findAll('blog_id=?',
+                                          [id],
+                                          orderBy='create_at desc')
+    for c in comments:
+        c.html_content = text2html(c.content)
+    blog.html_content = markdown2.markdown(blog.content)
+    return {
+        '__template__': 'blog.html',
+        'blog': blog,
+        'comments': comments
+    }
 
 
 @get('/')
@@ -124,6 +164,15 @@ _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$'
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
 
+@get('/manage/blogs/create')
+def manage_create_blog():
+    return {
+        '__template__': 'manage_blog_edit.html',
+        'id': '',
+        'action': '/api/blogs'
+    }
+
+
 @post('/api/users')
 def api_register_user(*, email, name, passwd):
     if not name or not name.strip():
@@ -153,3 +202,28 @@ def api_register_user(*, email, name, passwd):
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
+
+
+@get('/api/blogs/{id}')
+def api_get_blog(*, id):
+    blog = yield from Blog.find(id)
+    return blog
+
+
+@get('/api/blogs')
+def api_create_blog(request, *, name, summary, content):
+    check_admin(request)
+    if not name or name.strip():
+        raise APIValueError('name', 'name cannot be empty!')
+    if not summary or summary.strip():
+        raise APIValueError('summary', 'summary cannot be empty!')
+    if not content or content.strip():
+        raise APIValueError('content', 'content cannot be empty!')
+    blog = Blog(user_id=request.__user__.id,
+                user_name=request.__user__.name,
+                user_image=request.__user__.image,
+                name=name.strip(),
+                summary=summary.strip(),
+                content=content.strip())
+    yield from blog.save()
+    return blog
